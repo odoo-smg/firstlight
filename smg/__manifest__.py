ff9ccc16a1986382f845b004e35a32cc51761b21
odoo-smg <alexandresousa@smartrend.com>
2020-01-23 12:45:09 -0600

    Create smg
    smg

---
+++ smg/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SMG - Customizations",
+
+    'summary': """
+        This module intend to customize the functions and list to
+        Smartrend Manufacturing Group""",
+
+    'description': """
+        Customizations performed:
+        
+        Products:
+            * Product Name:       Validates unique description.
+            * Internal Reference: Validates unique code. Mandatory field. Replaces the field name to Product Code.
+            
+        Manufacturing:
+            * MRP Produce:
+                
+        
+    """,
+
+    'author': "Alexandre Sousa",
+    'website': "http://www.smartrendmfg.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'depends': ['stock'],
+
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/smg_product_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ smg/models/mrpproduce.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, api, models, exceptions
+
+class myproduceline(models.TransientModel):
+    _inherit = 'mrp.product.produce.line'
+    _check_company_auto = True
+
+    #lot_id = fields.Many2one('stock.production.lot', 'Lot/Serial Number', domain=lambda self: self._get_lot_domain(), check_company=True)
+    lot_id = fields.Many2one('stock.production.lot', 'Lot/Serial Number', check_company=True)
+
+    @api.onchange('lot_id')
+    def _lot_id_onchange(self):
+        lot_locations = self.env['stock.location'].search([('usage', '=', 'internal'), ('active', '=', True)]).ids
+        stock_id = self.env['stock.quant'].search([('product_id', '=', self.product_id.id),('quantity', '>', 0),('location_id', '=', lot_locations)]).mapped("lot_id").ids
+
+        if self.lot_id.id in stock_id:
+            return {
+                'domain': {
+                    'lot_id': [('id', 'in', stock_id)]
+                },
+            }
+        else:
+            return {
+                'warning': {
+                    'title': "Zero quantity",
+                    'message': "The Lot/Serial Number select has not stock available. Please select a valid Lot/Serial Number",
+                },
+                'domain': {
+                    'lot_id': [('id', 'in', stock_id)]
+                },
+            }
+
+    @api.constrains('lot_id')
+    def _check_stock_availability(self):
+        valid = True
+        for record in self:
+            lot_locations = self.env['stock.location'].search([('usage', '=', 'internal'), ('active', '=', True)]).ids
+            stock_id = self.env['stock.quant'].search([('product_id', '=', record.product_id.id), ('quantity', '>', 0),
+                                                       ('location_id', '=', lot_locations)]).mapped("lot_id").ids
+            if not record.lot_id.id in stock_id:
+                raise exceptions.ValidationError("The following Lot does not has quantity available in stock: %s" % record.lot_id.id)
+
+    @api.model
+    def _get_lot_domain(self):
+        lot_locations = self.env['stock.location'].search([('usage', '=', 'internal'), ('active', '=', True)]).ids
+        stock_id = self.env['stock.quant'].search([('product_id', '=', 39),('quantity', '>', 0),('location_id', '=', lot_locations)]).mapped("lot_id").ids
+        return [('id', 'in', stock_id)]
+
+
+
