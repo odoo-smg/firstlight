ec0876ec26f2d9a24acf3f0ef00d4e450a6ab256
odoo-smg <alexandresousa@smartrend.com>
2020-02-25 10:00:01 -0600

    Update __manifest__.py

--- smg/__manifest__.py
+++ smg/__manifest__.py
@@ -34,7 +34,7 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/WebAssetsBackend.xml',
     ],
     # only loaded in demonstration mode
