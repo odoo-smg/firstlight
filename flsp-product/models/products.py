e2cbf94d717aee8f341c08aa8fc7d5af25e0305e
odoo-smg <alexandresousa@smartrend.com>
2020-02-25 11:32:47 -0600

    Update products.py

--- flsp-product/models/products.py
+++ flsp-product/models/products.py
@@ -8,14 +8,6 @@ class Smgproduct(models.Model):
     _check_company_auto = True
 
     @api.model
-    def _default_nextpart(self):
-        self._cr.execute("select max(default_code) as code from product_product where default_code like '1%' and length(default_code) = 10 ")
-        retvalue = self._cr.fetchall()
-        returned_registre = retvalue[0]
-        self.default_code = self._get_next_part(returned_registre[0])
-        return self._get_next_part(returned_registre[0])
-
-    @api.model
     def _default_nextprefix(self):
         self._cr.execute("select max(default_code) as code from product_product where default_code like '1%' and length(default_code) = 10 ")
         retvalue = self._cr.fetchall()
@@ -23,7 +15,7 @@ class Smgproduct(models.Model):
         return self._get_next_prefix(returned_registre[0])
 
     # Change description and set it as mandatory
-    default_code = fields.Char(string="Internal Reference", default=_default_nextpart, readonly=True)
+    default_code = fields.Char(string="Internal Reference", readonly=True)
 
     legacy_code = fields.Char(string="Legacy Part #")
     flsp_part_prefix = fields.Char(string="Part # Prefix", default=_default_nextprefix)
@@ -31,30 +23,19 @@ class Smgproduct(models.Model):
 
     # constraints to validate code and description to be unique
     _sql_constraints = [
-        ('default_code_name_check_flsp6',
+        ('default_code_name_check_flsp5',
          'CHECK(name != default_code)',
          "The Name of the product should not be the product code"),
 
-        ('default_code_unique_flsp6',
+        ('default_code_unique_flsp5',
          'UNIQUE(default_code)',
          "The Product Code must be unique"),
 
-        ('name_unique_flsp6',
+        ('name_unique_flsp5',
          'UNIQUE(name)',
          "The Product name must be unique"),
     ]
 
-
-    @api.model
-    def _get_next_part(self, currpartnum):
-        retvalue = ''
-        if not currpartnum:
-            currpartnum = '100000-000'
-        if (currpartnum[0:1]!='1'):
-            retvalue = '100000-000'
-        else:
-            retvalue = str(int(currpartnum[0:6])+1)+"-000"
-        return retvalue
 
     @api.model
     def _get_next_prefix(self, currpartnum):
@@ -64,7 +45,7 @@ class Smgproduct(models.Model):
         if (currpartnum[0:1]!='1'):
             retvalue = '00001'
         else:
-            retvalue = str(int(currpartnum[0:5])+1)+" "
+            retvalue = ('00000'+str(int(currpartnum[1:6])+1))[-5:]
         return retvalue
 
 
@@ -109,7 +90,7 @@ class Smgproduct(models.Model):
             },
         }
 
-    def copy(self, defaul=None):
+    def copy(self, default=None):
         default = dict(default or {})
 
         if not(self.flsp_part_suffix):
@@ -122,14 +103,9 @@ class Smgproduct(models.Model):
         else:
             prefix = self.flsp_part_prefix
 
-        #return_val = '1'+('00000' + prefix.replace("_", ""))[-5:] + '-' + ('000' + suffix.replace("_", ""))[-3:]
-        self.default_code = '000000-000' #return_val
-        self.flsp_part_suffix = suffix
-        self.flsp_part_prefix = prefix
-        return {
-            'value': {
-                'default_code': return_val,
-                'flsp_part_suffix': suffix,
-                'flsp_part_prefix': prefix,
-            },
-        }
+        suffix = ('000'+str(int(suffix)))[-3:]
+
+        default['default_code'] = '1'+prefix+'-'+suffix
+        default['flsp_part_suffix'] = suffix
+        default['flsp_part_prefix'] = prefix
+        return super(Smgproduct, self).copy(default)
