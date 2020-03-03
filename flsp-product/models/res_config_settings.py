2878d4f628ba8fd583ffb0dfb318cfe838125c7e
odoo-smg <alexandresousa@smartrend.com>
2020-03-03 13:55:32 -0600

    Update res_config_settings.py

--- flsp-product/models/res_config_settings.py
+++ flsp-product/models/res_config_settings.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    flsp_part_init = fields.Char(related="company_id.flsp_part_init", default="1", readonly=False)
+    flsp_part_init = fields.Char(string="First Par", default="1", readonly=False)
     flsp_sec_param = fields.Integer(string="Second Par", default=1)
