204d2e253b9e10e368e868e19619fe276de834b2
odoo-smg <alexandresousa@smartrend.com>
2020-03-03 14:53:24 -0600

    Update res_config_settings.py

--- flsp-product/models/res_config_settings.py
+++ flsp-product/models/res_config_settings.py
@@ -10,13 +10,13 @@ class ResConfigSettings(models.TransientModel):
     #default_seats = fields.Integer(default_model='openacademy.session')
     #my_setting = fields.Char(string='My Setting')
 
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(
-            flsp_part_init=self.env['ir.config_parameter'].sudo().get_param('product_template.flsp_part_init')
-        )
-        return res
+    #def get_values(self):
+    #    res = super(ResConfigSettings, self).get_values()
+    #    res.update(
+    #        flsp_part_init=self.env['ir.config_parameter'].sudo().get_param('product_template.flsp_part_init')
+    #    )
+    #    return res
 
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('product_template.flsp_part_init', self.flsp_part_init)
+    #def set_values(self):
+    #    super(ResConfigSettings, self).set_values()
+    #    self.env['ir.config_parameter'].sudo().set_param('product_template.flsp_part_init', self.flsp_part_init)
