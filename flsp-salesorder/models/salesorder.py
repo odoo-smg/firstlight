36768aaa7cc1ad69dc8ff590978f87aef8ffbe4a
odoo-smg <alexandresousa@smartrend.com>
2020-03-03 10:25:31 -0600

    Update salesorder.py

--- flsp-salesorder/models/salesorder.py
+++ flsp-salesorder/models/salesorder.py
@@ -7,3 +7,11 @@ class flspsalesorder(models.Model):
     _inherit = 'sale.order'
 
     flsp_so_user_id = fields.Many2one('res.users', string="Salesperson 2")
+
+    @api.onchange('partner_id')
+    def flsp_partner_onchange(self):
+        return {
+            'value': {
+                'flsp_so_user_id': self.partner_id.flsp_user_id
+            },
+        }
