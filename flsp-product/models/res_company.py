501a354054a5a6656e876a6e85045c7e904e6b45
odoo-smg <alexandresousa@smartrend.com>
2020-03-03 14:29:55 -0600

    Delete res_company.py

--- flsp-product/models/res_company.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-class Company(models.Model):
-    _inherit = 'res.company'
-
-    #flsp_part_num_init = fields.Char(string="First digit part #")
