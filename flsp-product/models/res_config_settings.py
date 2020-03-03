5af7d6753617e6f94ce3b108352a2929a427cf72
odoo-smg <alexandresousa@smartrend.com>
2020-03-03 14:51:57 -0600

    Update res_config_settings.py

--- flsp-product/models/res_config_settings.py
+++ flsp-product/models/res_config_settings.py
@@ -4,24 +4,19 @@ from odoo import api, fields, models, modules
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    flsp_part_init = fields.Char(string="First Par")
+    dafault_part_init = fields.Char(default_model='product.template')
+    flsp_part_init = fields.Char(string="First Digit Part #")
 
-    #my_custom_field1_id = fields.Many2one('res.partner', string='For Customer')
-    #my_custom_field2_id = fields.Many2one('res.partner', string='For Supplier')
+    #default_seats = fields.Integer(default_model='openacademy.session')
+    #my_setting = fields.Char(string='My Setting')
 
-    @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         res.update(
-            flsp_part_init = self.env['ir.config_parameter'].sudo().get_param('flsp-product.flsp_part_init'),
+            flsp_part_init=self.env['ir.config_parameter'].sudo().get_param('product_template.flsp_part_init')
         )
         return res
 
-    @api.multi
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-        param = self.env['ir.config_parameter'].sudo()
-
-        field1 = self.flsp_part_init or False
-
-        param.set_param('flsp-product.flsp_part_init', field1)
+        self.env['ir.config_parameter'].sudo().set_param('product_template.flsp_part_init', self.flsp_part_init)
