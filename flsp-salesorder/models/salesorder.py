e08c288754b9dbc96aed1041ab1bf61f218c136f
odoo-smg <alexandresousa@smartrend.com>
2020-03-03 08:28:57 -0600

    Update salesorder.py

--- flsp-salesorder/models/salesorder.py
+++ flsp-salesorder/models/salesorder.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 
 
 class flspsalesorder(models.Model):
-    _inherit = 'sales.order'
+    _inherit = 'sale.order'
     _check_company_auto = True
 
     flsp_so_user_id = fields.Many2one('res.users', string="Salesperson 2")
