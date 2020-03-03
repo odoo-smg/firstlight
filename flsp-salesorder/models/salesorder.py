cc131cf381920d4dc3f3435acba0d7518fb4856d
odoo-smg <alexandresousa@smartrend.com>
2020-03-03 10:35:06 -0600

    Update salesorder.py

--- flsp-salesorder/models/salesorder.py
+++ flsp-salesorder/models/salesorder.py
@@ -8,10 +8,10 @@ class flspsalesorder(models.Model):
 
     flsp_so_user_id = fields.Many2one('res.users', string="Salesperson 2")
 
-    @api.onchange('partner_id')
-    def flsp_partner_onchange(self):
-        return {
-            'value': {
-                'flsp_so_user_id': self.partner_id.flsp_user_id.id
-            },
-        }
+    #@api.onchange('partner_id')
+    #def flsp_partner_onchange(self):
+    #    return {
+    #        'value': {
+    #            'flsp_so_user_id': self.partner_id.flsp_user_id.id
+    #        },
+    #    }
