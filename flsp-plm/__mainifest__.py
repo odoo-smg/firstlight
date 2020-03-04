c2edbccbc63a2d8326e295ee57a38eaa01e30125
odoo-smg <alexandresousa@smartrend.com>
2020-03-04 09:49:00 -0600

    Update __mainifest__.py

--- flsp-plm/__mainifest__.py
+++ flsp-plm/__mainifest__.py
@@ -20,7 +20,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Technical Settings',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
