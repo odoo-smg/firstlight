d320cb77d3fdbb8140865502c3abb77a24257505
odoo-smg <alexandresousa@smartrend.com>
2020-03-05 15:55:05 -0600

    Update flsp_mrp_report_bom.py

--- flsp-mrp/report/flsp_mrp_report_bom.py
+++ flsp-mrp/report/flsp_mrp_report_bom.py
@@ -9,7 +9,7 @@ class flsp_bomstrcreport(models.AbstractModel):
     def render_html(self,data=None):
         report_obj = self.env['report']
         print('>>>>>>>>>>.....', report_obj)
-        report = report_obj._get_report_from_name('flsp-mrp.flsp_mrp_report_bom')
+        report = report_obj._get_report_from_name('flsp-mrp.flsp_mrp_report_bom_view')
         print('>>>>>>>>>>', report)
         data_array = []
 
@@ -17,4 +17,4 @@ class flsp_bomstrcreport(models.AbstractModel):
             'data':data_array,
             }
 
-        return report_obj.render('flsp-mrp.flsp_mrp_report_bom', docargs)
+        return report_obj.render('flsp-mrp.flsp_mrp_report_bom_view', docargs)
