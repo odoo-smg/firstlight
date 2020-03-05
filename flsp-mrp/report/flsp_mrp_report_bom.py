98a3ffc0d8f79df2d6e8ede96c7d7cfd76d517c3
odoo-smg <alexandresousa@smartrend.com>
2020-03-05 16:12:44 -0600

    Update flsp_mrp_report_bom.py

--- flsp-mrp/report/flsp_mrp_report_bom.py
+++ flsp-mrp/report/flsp_mrp_report_bom.py
@@ -14,17 +14,17 @@ class flsp_bomstrcreport(models.AbstractModel):
             quantity = float(data.get('quantity', 1))
             for product_variant_id in candidates:
                 if data and data.get('childs'):
-                    doc = self._get_pdf_line(bom_id, product_id=product_variant_id, qty=quantity, child_bom_ids=json.loads(data.get('childs')))
+                    doc = [] #self._get_pdf_line(bom_id, product_id=product_variant_id, qty=quantity, child_bom_ids=json.loads(data.get('childs')))
                 else:
-                    doc = self._get_pdf_line(bom_id, product_id=product_variant_id, qty=quantity, unfolded=True)
+                    doc = [] #self._get_pdf_line(bom_id, product_id=product_variant_id, qty=quantity, unfolded=True)
                 doc['report_type'] = 'pdf'
                 doc['report_structure'] = data and data.get('report_type') or 'all'
                 docs.append(doc)
             if not candidates:
                 if data and data.get('childs'):
-                    doc = self._get_pdf_line(bom_id, qty=quantity, child_bom_ids=json.loads(data.get('childs')))
+                    doc = [] #self._get_pdf_line(bom_id, qty=quantity, child_bom_ids=json.loads(data.get('childs')))
                 else:
-                    doc = self._get_pdf_line(bom_id, qty=quantity, unfolded=True)
+                    doc = [] #self._get_pdf_line(bom_id, qty=quantity, unfolded=True)
                 doc['report_type'] = 'pdf'
                 doc['report_structure'] = data and data.get('report_type') or 'all'
                 docs.append(doc)
@@ -33,6 +33,49 @@ class flsp_bomstrcreport(models.AbstractModel):
             'doc_model': 'mrp.bom',
             'docs': docs,
         }
+
+    def _get_bom(self, bom_id=False, product_id=False, line_qty=False, line_id=False, level=False):
+        bom = self.env['mrp.bom'].browse(bom_id)
+        bom_quantity = line_qty
+        if line_id:
+            current_line = self.env['mrp.bom.line'].browse(int(line_id))
+            bom_quantity = current_line.product_uom_id._compute_quantity(line_qty, bom.product_uom_id)
+        # Display bom components for current selected product variant
+        if product_id:
+            product = self.env['product.product'].browse(int(product_id))
+        else:
+            product = bom.product_id or bom.product_tmpl_id.product_variant_id
+        if product:
+            attachments = self.env['mrp.document'].search(['|', '&', ('res_model', '=', 'product.product'),
+            ('res_id', '=', product.id), '&', ('res_model', '=', 'product.template'), ('res_id', '=', product.product_tmpl_id.id)])
+        else:
+            product = bom.product_tmpl_id
+            attachments = self.env['mrp.document'].search([('res_model', '=', 'product.template'), ('res_id', '=', product.id)])
+        operations = self._get_operation_line(bom.routing_id, float_round(bom_quantity / bom.product_qty, precision_rounding=1, rounding_method='UP'), 0)
+        company = bom.company_id or self.env.company
+        lines = {
+            'bom': bom,
+            'bom_qty': bom_quantity,
+            'bom_prod_name': product.display_name,
+            'currency': company.currency_id,
+            'product': product,
+            'code': bom and bom.display_name or '',
+            'price': product.uom_id._compute_price(product.with_context(force_company=company.id).standard_price, bom.product_uom_id) * bom_quantity,
+            'total': sum([op['total'] for op in operations]),
+            'level': level or 0,
+            'operations': operations,
+            'operations_cost': sum([op['total'] for op in operations]),
+            'attachments': attachments,
+            'operations_time': sum([op['duration_expected'] for op in operations])
+        }
+        components, total = self._get_bom_lines(bom, bom_quantity, product, line_id, level)
+        lines['components'] = components
+        lines['total'] += total
+        return lines
+
+    def _get_pdf_line(self, bom_id, product_id=False, qty=1, child_bom_ids=[], unfolded=False):
+
+        data = self._get_bom(bom_id=bom_id, product_id=product_id.id, line_qty=qty)
 
     @api.model
     def render_html(self,data=None):
