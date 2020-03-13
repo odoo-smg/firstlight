738860746356f938d9ae2488e17e3220228d80e0
odoo-smg <alexandresousa@smartrend.com>
2020-03-13 15:38:41 -0500

    Update products.py

--- flsp-product/models/products.py
+++ flsp-product/models/products.py
@@ -26,15 +26,15 @@ class Smgproduct(models.Model):
 
     # constraints to validate code and description to be unique
     _sql_constraints = [
-        ('default_code_name_check_flsp5',
+        ('default_code_name_check_flsp6',
          'CHECK(name != default_code)',
          "The Name of the product should not be the product code"),
 
-        ('default_code_unique_flsp5',
+        ('default_code_unique_flsp6',
          'UNIQUE(default_code)',
          "The Product Code must be unique"),
 
-        ('name_unique_flsp5',
+        ('name_unique_flsp6',
          'UNIQUE(name)',
          "The Product name must be unique"),
     ]
