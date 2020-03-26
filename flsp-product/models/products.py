e81ab846fa2a92b88058fcab223b4e1714f6ea60
odoo-smg <alexandresousa@smartrend.com>
2020-03-26 14:33:57 -0500

    Update products.py

--- flsp-product/models/products.py
+++ flsp-product/models/products.py
@@ -2,6 +2,12 @@
 
 from odoo import fields, models, api, exceptions
 
+class smgproductprd(models.Model)
+    _inherit = 'product.product'
+    _check_company_auto = True
+
+    flsp_acc_valid   = fields.Boolean(string="Acconting Validated", readonly=True)
+
 
 class Smgproduct(models.Model):
     _inherit = 'product.template'
