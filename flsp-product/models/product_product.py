9093c0b58b23acb5a85d852166eec68f4ca1dbc5
odoo-smg <alexandresousa@smartrend.com>
2020-03-26 16:18:49 -0500

    Update product_product.py

--- flsp-product/models/product_product.py
+++ flsp-product/models/product_product.py
@@ -10,10 +10,11 @@ class smgproductprd(models.Model):
     flsp_acc_valid   = fields.Boolean(string="Acconting Validated", readonly=True)
 
     def button_acc_valid(self):
+        self.product_tmpl_id.flsp_acc_valid = True
         #product_id = self.env['product.product'].browse(self.env.context['default_product_id'])
-        current_product = self.env['product.template'].search([('id', '=', self.product_tmpl_id)])
-        if current_product:
-            current_product.write({'flsp_acc_valid': True}, context=None)
+        #current_product = self.env['product.template'].search([('id', '=', self.product_tmpl_id)])
+        #if current_product:
+        #    current_product.write({'flsp_acc_valid': True}, context=None)
         return self.write({'flsp_acc_valid': True})
 
     def button_acc_valid_off(self):
