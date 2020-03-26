98ce991edd08177fe4361a2f16d03984b07156fd
odoo-smg <alexandresousa@smartrend.com>
2020-03-26 14:57:54 -0500

    Update product_product.py

--- flsp-product/models/product_product.py
+++ flsp-product/models/product_product.py
@@ -8,3 +8,9 @@ class smgproductprd(models.Model):
     _check_company_auto = True
 
     flsp_acc_valid   = fields.Boolean(string="Acconting Validated", readonly=True)
+
+    def button_acc_valid(self):
+        return self.write({'flsp_acc_valid': True})
+
+    def button_acc_valid_off(self):
+        return self.write({'flsp_acc_valid': False})
