4b7606746336ccd515a17e7ae89bb73eed6717bc
odoo-smg <alexandresousa@smartrend.com>
2020-04-28 11:32:07 -0500

    Sales Approval - CAM validation

---
+++ flspsaleapproval/models/flsp_sale_wizard.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Saleflspwizard(models.TransientModel):
+    _name = 'flspsaleapproval.saleflspwizard'
+    _description = "Wizard: testing my wizard"
+
+    @api.model
+    def default_get(self, fields):
+        res = super(Saleflspwizard, self).default_get(fields)
+        sale_order = self.env['sale.order']
+        order_id = self.env.context.get('default_order_id') or self.env.context.get('active_id')
+        if order_id:
+            sale_order = self.env['sale.order'].browse(order_id)
+        if sale_order.exists():
+            if 'order_id' in fields:
+                res['order_id'] = sale_order.id
+            if 'validity_date' in fields:
+                res['validity_date'] = sale_order.validity_date
+            if 'partner_id' in fields:
+                res['partner_id'] = sale_order.partner_id.id
+            #if 'order_line' in fields:
+            #res['order_line'] = sale_order.order_line
+
+        approval_list = []
+        for so_line in [l for l in sale_order.order_line]:
+            approval_list.append([0, 0, {
+                'flsp_sale_line_product_id': so_line.id,
+                'sequence': so_line.sequence,
+                'product_template_id': so_line.product_template_id.id,
+                'product_uom_qty': so_line.product_uom_qty,
+                'product_uom': so_line.product_template_id.uom_id.id,
+                'price_unit': so_line.price_unit,
+                'discount': so_line.discount,
+                'sale_order_line_id': so_line.id,
+                'tax_id': so_line.tax_id.id,
+                'order_id': so_line.order_id.id,
+                'price_subtotal': so_line.price_subtotal,
+            }])
+        res['flsp_order_line_ids'] = approval_list
+        res = self._convert_to_write(res)
+        return res
+
+
+    order_id = fields.Many2one('sale.order', string='Order Reference', readonly=True)
+    partner_id = fields.Many2one('res.partner', string='Customer', readonly=True)
+    validity_date = fields.Date(string='Expiration')
+    flsp_order_line_ids = fields.One2many('flsp.so.line', 'flsp_sale_line_product_id', string='Components')
+
+
+    def flsp_approve(self):
+        self.ensure_one()
+        for approval_line in self.flsp_order_line_ids:
+            approval_line.sale_order_line_id.write({
+                'discount': approval_line.discount,
+            })
+            approval_line.sale_order_line_id.write({
+                'price_unit': approval_line.price_unit,
+            })
+        self.order_id.write({'flsp_state': 'approved', })
+        self.order_id.write({'flsp_approval_approved': True, })
+
+        return {'type': 'ir.actions.act_window_close'}
+
+        #self.order_id.write({'validity_date': self.validity_date,})
+
+    def flsp_open_sale_wizard(self):
+        self.order_line.order_id |= self.order_id
+        return {}
+
+class FlspSOline(models.TransientModel):
+    """Sales Approval"""
+    _name = "flsp.so.line"
+    _description = 'Edit Line on Sales Approval'
+
+    flsp_sale_line_product_id = fields.Many2one('flspsaleapproval.saleflspwizard')
+    sale_order_line_id = fields.Many2one('sale.order.line', string='Sales Order Line')
+    order_id = fields.Many2one('sale.order', string='Order Reference', required=True, ondelete='cascade', index=True, copy=False)
+
+    sequence = fields.Integer(string='Sequence', default=10)
+    product_template_id = fields.Many2one('product.template', string='Product Template')
+    product_uom_qty = fields.Float(string='Quantity', digits='Product Unit of Measure', default=1.0)
+    product_uom = fields.Many2one('uom.uom', string='Unit of Measure')
+    product_uom_category_id = fields.Many2one(related='product_template_id.uom_id.category_id', readonly=True)
+    price_unit = fields.Float('Unit Price', digits='Product Price', default=0.0)
+    discount = fields.Float(string='Discount (%)', digits='Discount', default=0.0)
+    price_subtotal = fields.Monetary(compute='_compute_amount', string='Subtotal', readonly=True, store=True)
+    tax_id = fields.Many2many('account.tax', string='Taxes',
+                              domain=['|', ('active', '=', False), ('active', '=', True)])
+    currency_id = fields.Many2one(related='order_id.currency_id', depends=['order_id'], store=True, string='Currency',
+                                  readonly=True)
+    def get_approval_data(self):
+        self.ensure_one()
+        return {
+            'flsp_sale_line_product_id': self.flsp_sale_line_product_id.id,
+            'product_template_id': self.product_template_id.id,
+            'sequence': self.sequence,
+            'product_uom_qty': self.product_uom_qty,
+            'price_unit': self.price_unit,
+            'discount': self.discount,
+            'sale_order_id': self.flsp_sale_line_product_id.order_id.id,
+            'sale_order_line_id': self.sale_order_line_id.id,
+        }
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        for line in self:
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_template_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_subtotal': taxes['total_excluded'],
+            })
---
+++ flspsaleapproval/models/flsporderline.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    sale_order_option_ids = fields.One2many('sale.order.option', 'line_id', 'Optional Products Lines')
+
+class SaleOrderOption(models.Model):
+    _name = "sale.order.option"
+    _description = "Sale Options"
+    _order = 'sequence, id'
+
+    is_present = fields.Boolean(string="Present on Quotation",
+                           help="This field will be checked if the option line's product is "
+                                "already present in the quotation.",
+                           compute="_compute_is_present", search="_search_is_present")
+    order_id = fields.Many2one('sale.order', 'Sales Order Reference', ondelete='cascade', index=True)
+    line_id = fields.Many2one('sale.order.line', ondelete="set null", copy=False)
+    name = fields.Text('Description', required=True)
+    product_id = fields.Many2one('product.product', 'Product', required=True, domain=[('sale_ok', '=', True)])
+    price_unit = fields.Float('Unit Price', required=True, digits='Product Price')
+    discount = fields.Float('Discount (%)', digits='Discount')
+    uom_id = fields.Many2one('uom.uom', 'Unit of Measure ', required=True, domain="[('category_id', '=', product_uom_category_id)]")
+    product_uom_category_id = fields.Many2one(related='product_id.uom_id.category_id', readonly=True)
+    quantity = fields.Float('Quantity', required=True, digits='Product UoS', default=1)
+    sequence = fields.Integer('Sequence', help="Gives the sequence order when displaying a list of optional products.")
+
+    @api.depends('line_id', 'order_id.order_line', 'product_id')
+    def _compute_is_present(self):
+        # NOTE: this field cannot be stored as the line_id is usually removed
+        # through cascade deletion, which means the compute would be false
+        for option in self:
+            option.is_present = bool(option.order_id.order_line.filtered(lambda l: l.product_id == option.product_id))
+
+    def _search_is_present(self, operator, value):
+        if (operator, value) in [('=', True), ('!=', False)]:
+            return [('line_id', '=', False)]
+        return [('line_id', '!=', False)]
+
+    @api.onchange('product_id', 'uom_id')
+    def _onchange_product_id(self):
+        if not self.product_id:
+            return
+        product = self.product_id.with_context(lang=self.order_id.partner_id.lang)
+        self.price_unit = product.list_price
+        self.name = product.get_product_multiline_description_sale()
+        self.uom_id = self.uom_id or product.uom_id
+        pricelist = self.order_id.pricelist_id
+        if pricelist and product:
+            partner_id = self.order_id.partner_id.id
+            self.price_unit = pricelist.with_context(uom=self.uom_id.id).get_product_price(product, self.quantity, partner_id)
+        domain = {'uom_id': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+        return {'domain': domain}
+
+    def button_add_to_order(self):
+        self.add_option_to_order()
+
+    def add_option_to_order(self):
+        self.ensure_one()
+
+        sale_order = self.order_id
+
+        if sale_order.state not in ['draft', 'sent']:
+            raise UserError(_('You cannot add options to a confirmed order.'))
+
+        values = self._get_values_to_add_to_order()
+        order_line = self.env['sale.order.line'].create(values)
+        order_line._compute_tax_id()
+
+        self.write({'line_id': order_line.id})
+
+    def _get_values_to_add_to_order(self):
+        self.ensure_one()
+        return {
+            'order_id': self.order_id.id,
+            'price_unit': self.price_unit,
+            'name': self.name,
+            'product_id': self.product_id.id,
+            'product_uom_qty': self.quantity,
+            'product_uom': self.uom_id.id,
+            'discount': self.discount,
+            'company_id': self.order_id.company_id.id,
+        }
