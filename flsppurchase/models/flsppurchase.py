07b26f1d68f7c76e9f72642c3a0a4347705c4758
odoo-smg <alexandresousa@smartrend.com>
2020-05-05 15:12:53 -0500

    vendor code

--- flsppurchase/models/flsppurchase.py
+++ flsppurchase/models/flsppurchase.py
@@ -9,7 +9,7 @@ class flsppurchase(models.Model):
 
     flsp_vendor_code = fields.Char('Vendor Product Code', compute='_calc_vendor_code')
 
-    @api.depends('product_qty', 'product_uom', 'product_id')
+    @api.depends('product_id')
     def _calc_vendor_code(self):
         self.flsp_vendor_code = '1'
         for line in self:
