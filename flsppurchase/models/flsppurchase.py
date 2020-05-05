08ce45110d2958bcf041b7b6db4797f211c60549
odoo-smg <alexandresousa@smartrend.com>
2020-05-05 13:56:51 -0500

    Update flsppurchase.py

--- flsppurchase/models/flsppurchase.py
+++ flsppurchase/models/flsppurchase.py
@@ -7,10 +7,26 @@ class flsppurchase(models.Model):
     _inherit = 'purchase.order.line'
     _check_company_auto = True
 
-    flsp_vendor_code = fields.Char('Vendor Product Code')
+    flsp_vendor_code = fields.Char('Vendor Product Code', compute='_calc_vendor_code')
 
     @api.onchange('product_qty', 'product_uom', 'product_id')
     def _onchange_flsp_product(self):
+        if not self.product_id:
+            return
+        params = {'order_id': self.order_id}
+        seller = self.product_id._select_seller(
+            partner_id=self.partner_id,
+            quantity=self.product_qty,
+            date=self.order_id.date_order and self.order_id.date_order.date(),
+            uom_id=self.product_uom,
+            params=params)
+
+        if not seller:
+            return
+        self.flsp_vendor_code = seller.product_code
+
+    @api.depends('product_qty', 'product_uom', 'product_id')
+    def _calc_vendor_code(self):
         if not self.product_id:
             return
         params = {'order_id': self.order_id}
