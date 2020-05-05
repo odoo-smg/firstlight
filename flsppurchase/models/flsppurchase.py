4a2489152e88682d1a5c65855fb63b82819b327b
odoo-smg <alexandresousa@smartrend.com>
2020-05-05 14:37:13 -0500

    Update flsppurchase.py

--- flsppurchase/models/flsppurchase.py
+++ flsppurchase/models/flsppurchase.py
@@ -9,27 +9,15 @@ class flsppurchase(models.Model):
 
     flsp_vendor_code = fields.Char('Vendor Product Code', compute='_calc_vendor_code')
 
-    @api.onchange('product_qty', 'product_uom', 'product_id')
-    def _onchange_flsp_product(self):
-        if not self.product_id:
-            return
-        params = {'order_id': self.order_id}
-        seller = self.product_id._select_seller(
-            partner_id=self.partner_id,
-            quantity=self.product_qty,
-            date=self.order_id.date_order and self.order_id.date_order.date(),
-            uom_id=self.product_uom,
-            params=params)
-
-        if not seller:
-            return
-        self.flsp_vendor_code = seller.product_code
-
     @api.depends('product_qty', 'product_uom', 'product_id')
     def _calc_vendor_code(self):
         params = {'order_id': self.order_id}
         for line in self:
-            if line.flsp_vendor_code:
-                line.flsp_vendor_code = line.flsp_vendor_code+1
-            else:
-                line.flsp_vendor_code = 1
+            seller = line.product_id._select_seller(
+                partner_id=line.partner_id,
+                quantity=line.product_qty,
+                date=line.order_id.date_order and self.order_id.date_order.date(),
+                uom_id=line.product_uom,
+                params=params)
+            if seller:
+                line.flsp_vendor_code = seller.product_code
