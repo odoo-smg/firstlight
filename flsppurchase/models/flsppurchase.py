4b2bf38a2f679bba22753750c17dc97adb33a9a5
odoo-smg <alexandresousa@smartrend.com>
2020-05-05 15:19:54 -0500

    Update flsppurchase.py

--- flsppurchase/models/flsppurchase.py
+++ flsppurchase/models/flsppurchase.py
@@ -11,7 +11,15 @@ class flsppurchase(models.Model):
 
     @api.depends('product_id')
     def _calc_vendor_code(self):
-        self.flsp_vendor_code = '1'
+        seller = self.product_id._select_seller(
+            partner_id=self.partner_id,
+            quantity=self.product_qty,
+            date=self.order_id.date_order and self.order_id.date_order.date(),
+            uom_id=self.product_uom)
+        if seller:
+            self.flsp_vendor_code = seller.product_code
+        else:
+            self.flsp_vendor_code = '1'
 #        for line in self:
 #            if not line.flsp_vendor_code:
 #                line.flsp_vendor_code = '2'
