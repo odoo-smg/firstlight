caf0c8793d388e0b2f8f65554f1ed57f967dfdf3
odoo-smg <alexandresousa@smartrend.com>
2020-05-05 15:50:30 -0500

    Update flsppurchase.py

--- flsppurchase/models/flsppurchase.py
+++ flsppurchase/models/flsppurchase.py
@@ -11,18 +11,32 @@ class flsppurchase(models.Model):
 
     @api.depends('product_id')
     def _calc_vendor_code(self):
-        self.ensure_one()
-        for line in self:
-            seller = line.product_id._select_seller(
-                partner_id=line.partner_id,
-                quantity=line.product_qty,
-                date=line.order_id.date_order and line.order_id.date_order.date(),
-                uom_id=line.product_uom)
-            if seller:
-                line.flsp_vendor_code = seller.product_code
-            else:
-                line.flsp_vendor_code = '1'
-#            if not line.flsp_vendor_code:
-#                line.flsp_vendor_code = '2'
-#            else:
-#                line.flsp_vendor_code = '3'
+        date = fields.Date.context_today(self)
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        res = self.env['product.supplierinfo']
+        sellers = self._prepare_sellers(False)
+        if self.env.context.get('force_company'):
+            sellers = sellers.filtered(lambda s: not s.company_id or s.company_id.id == self.env.context['force_company'])
+
+        for seller in sellers:
+            quantity_uom_seller = self.product_qty
+            if quantity_uom_seller and uom_id and uom_id != seller.product_uom:
+                quantity_uom_seller = uom_id._compute_quantity(quantity_uom_seller, seller.product_uom)
+            if seller.date_start and seller.date_start > date:
+                continue
+            if seller.date_end and seller.date_end < date:
+                continue
+            if partner_id and seller.name not in [self.partner_id, self.partner_id.parent_id]:
+                continue
+            if float_compare(quantity_uom_seller, seller.min_qty, precision_digits=precision) == -1:
+                continue
+            if seller.product_id and seller.product_id != self.product_id:
+                continue
+            if not res or res.name == seller.name:
+                res |= seller
+        vendor = res.sorted('price')[:1]
+
+        if vendor:
+            line.flsp_vendor_code = vendor.product_code
+        else:
+            line.flsp_vendor_code = '1'
