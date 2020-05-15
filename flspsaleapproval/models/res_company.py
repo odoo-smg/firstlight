02a45057bec6ce454473eb09f5ae6f7a1fc3584e
odoo-smg <alexandresousa@smartrend.com>
2020-05-15 18:29:37 -0500

    flsp_gst_number back

--- flspsaleapproval/models/res_company.py
+++ flspsaleapproval/models/res_company.py
@@ -6,6 +6,7 @@ from odoo import api, fields, models
 
 class Flspcompany(models.Model):
     _inherit = "res.company"
+    _check_company_auto = True
 
     so_flsp_max_percent_approval = fields.Float(string="Max Discount Allowed", help="Minimum discount percent allowed")
-    _columns = {'flsp_gst_number':fields.Char('GST Reg No.', help="GST Reg No.", groups="base.group_system"),}
+    flsp_gst_number = fields.Char('GST Reg No2.', help="GST Reg No.", groups="base.group_system")
--- flspsaleapproval/models/res_config_settings.py
+++ flspsaleapproval/models/res_config_settings.py
@@ -6,7 +6,7 @@ class ResConfigSettings(models.TransientModel):
 
     flsp_sales_discount_approval = fields.Boolean(string="Sales Order Approval")
     so_flsp_max_percent_approval = fields.Float(related='company_id.so_flsp_max_percent_approval', string="Max Discount Allowed", readonly=False)
-    flsp_gst_number = fields.Char(related="company_id.flsp_gst_number", readonly=False)
+
 
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
