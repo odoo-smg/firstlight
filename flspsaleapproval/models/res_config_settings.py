a98e6796d993444b7833bb801f84386f652d8f90
odoo-smg <alexandresousa@smartrend.com>
2020-05-15 17:53:28 -0500

    Update res_config_settings.py

--- flspsaleapproval/models/res_config_settings.py
+++ flspsaleapproval/models/res_config_settings.py
@@ -6,20 +6,15 @@ class ResConfigSettings(models.TransientModel):
 
     flsp_sales_discount_approval = fields.Boolean(string="Sales Order Approval")
     so_flsp_max_percent_approval = fields.Float(related='company_id.so_flsp_max_percent_approval', string="Max Discount Allowed", readonly=False)
-    flsp_gst_reg_no = fields.Char(related="company_id.flsp_gst_reg_no", readonly=False)
+    #flsp_gst_reg_no = fields.Char(related="company_id.flsp_gst_reg_no", readonly=False)
 
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         res.update(
             flsp_sales_discount_approval=self.env['ir.config_parameter'].sudo().get_param('flsp_sales_discount_approval')
-            flsp_gst_reg_no=self.env['ir.config_parameter'].sudo().get_param('flsp_gst_reg_no')
         )
         return res
 
     def set_values(self):
         super(ResConfigSettings, self).set_values()
         self.env['ir.config_parameter'].sudo().set_param('flsp_sales_discount_approval', self.flsp_sales_discount_approval)
-
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('flsp_gst_reg_no', self.flsp_gst_reg_no)
