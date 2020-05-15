a61740fa83a7d8cabc4b1f7ccacceddc285a7bd8
odoo-smg <alexandresousa@smartrend.com>
2020-05-15 17:07:52 -0500

    gst settings

--- flspsaleapproval/models/res_company.py
+++ flspsaleapproval/models/res_company.py
@@ -8,6 +8,4 @@ class Flspcompany(models.Model):
     _inherit = "res.company"
 
     so_flsp_max_percent_approval = fields.Float(string="Max Discount Allowed", help="Minimum discount percent allowed")
-    #flsp_gst_reg_no = fields.Char('GST Reg No.', help="GST Reg No.", groups="base.group_system")
-    _columns = {'flsp_gst_reg_no':fields.Char('GST Reg No.', help="GST Reg No.", groups="base.group_system"),}
-    
+    flsp_gst_reg_no = fields.Char('GST Reg No.', help="GST Reg No.", groups="base.group_system")
