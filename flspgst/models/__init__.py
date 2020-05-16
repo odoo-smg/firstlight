7407baab0ff529c6df44718128dea47167814bab
odoo-smg <alexandresousa@smartrend.com>
2020-05-16 10:32:58 -0500

    new module

---
+++ flspgst/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import res_company_gst
+from . import res_config_settings_gst
--- flspsaleapproval/models/__init__.py
+++ flspsaleapproval/models/__init__.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from . import res_company
-from . import res_company_gst
-from . import flspsale
-from . import res_config_settings
-from . import res_config_settings_gst
-from . import flsporderline
+
 from . import flsp_sale_wizard
 from . import flsp_sale_message
 from . import res_partner
 from . import flsp_pricelist
+from . import flspsale
+from . import res_config_settings
+from . import flsporderline
+from . import res_company
