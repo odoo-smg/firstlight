79aed133a77362fa7c8829f6af5e3df304e2a2dc
odoo-smg <alexandresousa@smartrend.com>
2020-05-16 10:24:59 -0500

    Update res_company_gst.py

--- flspsaleapproval/models/res_company_gst.py
+++ flspsaleapproval/models/res_company_gst.py
@@ -6,6 +6,5 @@ from odoo import api, fields, models
 
 class Flspgstcompany(models.Model):
     _inherit = "res.company"
-    _check_company_auto = True
 
-    flsp_gst_number = fields.Char('GST Reg No2.', help="GST Reg No.", groups="base.group_system")
+    flsp_gst_number = fields.Char('GST Reg No.', help="GST Reg No.", groups="base.group_system")
