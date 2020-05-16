35f3a9e00a144e3586024d1d64c9df5c71391e61
odoo-smg <alexandresousa@smartrend.com>
2020-05-16 10:23:12 -0500

    settings

--- flspsaleapproval/models/__init__.py
+++ flspsaleapproval/models/__init__.py
@@ -4,6 +4,7 @@ from . import res_company
 from . import res_company_gst
 from . import flspsale
 from . import res_config_settings
+from . import res_config_settings_gst
 from . import flsporderline
 from . import flsp_sale_wizard
 from . import flsp_sale_message
