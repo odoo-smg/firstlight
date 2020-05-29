b6dcf9b7e0c4f8814c075033b1eb8ca34b5aa0c7
odoo-smg <alexandresousa@smartrend.com>
2020-05-29 16:43:34 -0500

    trigger save product

--- flspacc/models/accpayment.py
+++ flspacc/models/accpayment.py
@@ -32,3 +32,39 @@ class flsp_payment(models.Model):
         else:
             self.amount_required = amount_categ_total * flsp_percent_sppepp / 100
         self.currency_so_id = self.flsp_quote_id.currency_id.id
+
+
+    def button_flsp_confirm(self):
+        if self.flsp_quote_id:
+            so = env['sale.order'].search([('id', '=', record.flsp_quote_id.id)])
+            if so:
+            	so['flsp_amount_deposit'] = record.amount
+              ## 3% credit card payment
+            	if record.credit_card_payment:
+            	  flspsppepp_category_id = env.company.flspsppepp_category_id
+            	  flsp_percent_sppepp = env.company.flsp_percent_sppepp
+            	  flspsppepp_product_id = env.company.flspsppepp_product_id
+            	  sale_line_obj = env['sale.order.line']
+            	  amount_categ_total = 0
+            	  for line in record.flsp_quote_id.order_line:
+            	    if line.product_id.categ_id == flspsppepp_category_id:
+            	      amount_categ_total += line.price_subtotal
+
+            	  context = {'lang': so.partner_id.lang}
+            	  analytic_tag_ids = []
+            	  for line in so.order_line:
+            	    analytic_tag_ids = [(4, analytic_tag.id, None) for analytic_tag in line.analytic_tag_ids]
+
+            	  sale_line_obj.create({
+                    'name': '3% Increase CC Payment: %s',
+                    'price_unit': amount_categ_total * flsp_percent_sppepp / 100 *3/100,
+                    'product_uom_qty': 0.0,
+                    'order_id': so.id,
+                    'discount': 0.0,
+                    'product_uom': flspsppepp_product_id.product_tmpl_id.uom_id.id,
+                    'product_id': flspsppepp_product_id.id,
+                    'analytic_tag_ids': analytic_tag_ids,
+            	  })
+        action = self.action_confirm()
+
+        return action
