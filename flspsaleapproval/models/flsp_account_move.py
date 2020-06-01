03d6e5deef2adbecf23031d0a0a24501e565d2e5
odoo-smg <alexandresousa@smartrend.com>
2020-06-01 12:55:40 -0500

    Invoice

---
+++ flspsaleapproval/models/flsp_account_move.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class flspaccountmove(models.Model):
+    _inherit = 'account.move'
+    _check_company_auto = True
+
+    #def _get_reconciled_info_JSON_values(self):
+    def _get_sale_order_info_JSON_values(self):
+        self.ensure_one()
+        #excluded_move_ids = AccountMoveLine.search(AccountMoveLine._get_suspense_moves_domain() + [('move_id', 'in', self.ids)]).mapped('move_id').ids
+        #ids = self.env['account.move.line'].search(domain).mapped('statement_line_id').ids
+        #reverse_entries = self.env['account.move'].search([('reversed_entry_id', '=', self.id)])
+
+
+        so = self.env['sale.order'].search([('name', '=', self.invoice_origin.strip())])
+
+        reconciled_vals = []
+        for rec in so:
+            reconciled_vals.append({
+                'name': rec.name,
+                'date_order': rec.date_order,
+                'flsp_ship_via': rec.flsp_ship_via,
+                'payment_term': rec.payment_term_id.note,
+            })
+        return reconciled_vals
