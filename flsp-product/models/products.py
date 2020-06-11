32ddfb7efe87119e3e605aff4ac9e58fde6f0ca7
odoo-smg <alexandresousa@smartrend.com>
2020-06-11 11:54:04 -0500

    Update products.py

--- flsp-product/models/products.py
+++ flsp-product/models/products.py
@@ -146,5 +146,7 @@ class Smgproduct(models.Model):
         default['default_code'] = default_init+prefix+'-'+suffix
         default['flsp_part_suffix'] = suffix
         default['flsp_part_prefix'] = prefix
-        default['flsp_plm_valid'] = False
+        if 'flsp_plm_valid' in self.env['product.template']._fields:
+            default['flsp_plm_valid'] = False
+
         return super(Smgproduct, self).copy(default)
