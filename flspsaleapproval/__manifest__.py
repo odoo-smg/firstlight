492ceb026af6e5e05b2aa17a0ec0ab9b8db9547a
odoo-smg <alexandresousa@smartrend.com>
2020-06-12 14:52:04 -0500

    removed par

--- flspsaleapproval/__manifest__.py
+++ flspsaleapproval/__manifest__.py
@@ -27,16 +27,5 @@
 
     # always loaded
     'data': [
-        'views/flsp_sale_view.xml',
-        'views/flsp_sale_settings_view.xml',
-        'views/flsp_sale_optional_view.xml',
-        'views/flsp_sale_wizard_view.xml',
-        'views/flsp_sale_message_view.xml',
-        'views/flsp_sale_partner.xml',
-        'views/flsp_sale_pricelist_view.xml',
-        'report/flsp_sale_report.xml',
-        'report/flsp_invoice_report.xml',
-        'report/flsp_invoice_report_withoutpay.xml',
-        'views/flsp_sppepp_message_view.xml',
     ],
 }
