cd42cdc6a60e20a2a10f6e7825d81c4def5b3be0
odoo-smg <alexandresousa@smartrend.com>
2020-06-12 14:50:35 -0500

    Update __init__.py

--- flspsaleapproval/models/__init__.py
+++ flspsaleapproval/models/__init__.py
@@ -7,7 +7,7 @@ from . import res_partner
 from . import flsp_pricelist
 from . import flspsale
 from . import flsporderline
-from . import res_company
-from . import res_config_settings
+#from . import res_company
+#from . import res_config_settings
 from . import flsp_sppepp
 from . import flsp_account_move
