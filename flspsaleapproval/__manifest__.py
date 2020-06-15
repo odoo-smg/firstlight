fcb08a4bcde8c7ee2de61a5471a6a39faaaa267a
odoo-smg <alexandresousa@smartrend.com>
2020-06-15 16:30:52 -0500

    Update __manifest__.py

--- flspsaleapproval/__manifest__.py
+++ flspsaleapproval/__manifest__.py
@@ -22,8 +22,9 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account'],
+    'depends': ['base'],
     'depends': ['sale'],
+    'depends': ['account'],
 
     # always loaded
     'data': [
