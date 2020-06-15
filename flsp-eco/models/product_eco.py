26379bc0f6a844d5e29d05d2346e06e1b87b4b6f
odoo-smg <alexandresousa@smartrend.com>
2020-06-15 15:10:16 -0500

    flsp-eco

---
+++ flsp-eco/models/product_eco.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class flspproducts(models.Model):
+    _inherit = 'product.template'
+    _check_company_auto = True
+
+    # New fields to control ECO enforcement
+    flsp_eco_enforce = fields.Many2one('mrp.eco', string="ECO", store=False)
+    flsp_plm_valid   = fields.Boolean(string="PLM Validated")
+
+    @api.onchange('flsp_eco_enforce')
+    def flsp_eco_enforce_onchange(self):
+        self.flsp_plm_valid = False
+        return {
+            'value': {
+                'flsp_plm_valid': False
+            },
+        }
