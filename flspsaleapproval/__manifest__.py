542eaa46edb0db257f9dc6ddb570ce67dcd9a08d
odoo-smg <alexandresousa@smartrend.com>
2020-06-15 12:45:03 -0500

    removed freight

--- flspsaleapproval/__manifest__.py
+++ flspsaleapproval/__manifest__.py
@@ -39,6 +39,8 @@
         'report/flsp_invoice_report.xml',
         'report/flsp_invoice_report_withoutpay.xml',
         'views/flsp_sppepp_message_view.xml',
-        'views/flsp_choose_delivery.xml',
+
     ],
+#        'views/flsp_choose_delivery.xml',
+
 }
