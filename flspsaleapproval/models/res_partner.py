f207fb48b28287c9198f53688c5456f3ff832ae7
odoo-smg <alexandresousa@smartrend.com>
2020-06-26 16:16:11 -0500

    Update res_partner.py

--- flspsaleapproval/models/res_partner.py
+++ flspsaleapproval/models/res_partner.py
@@ -7,10 +7,15 @@ class flspsalespartner(models.Model):
     _inherit = 'res.partner'
     _check_company_auto = True
 
-    flsp_sale_currency = fields.Many2one('res.currency', string='Sale Currency', required=True)
+    @api.model
+    def _default_flsp_sale_currency(self):
+        currency_id = self.env['res.currency'].search([('name',  '=', 'USD')])
+        return currency_id
+
+    flsp_sale_currency = fields.Many2one('res.currency', string='Sale Currency', default=_default_flsp_sale_currency, required=True)
     flsp_sale_type = fields.Selection([
         ('1', 'OEM'),
         ('2', 'Dealer'),
         ('3', 'School'),
         ('4', 'Contractor'),
-        ], string='Sale Group',  required=True)
+        ], string='Sale Group',  default='3', required=True)
