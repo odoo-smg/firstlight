b23c49478ac1f51ce25dc5b047668050c34cd222
odoo-smg <alexandresousa@smartrend.com>
2020-07-03 16:08:04 -0500

    Update flsp_customer.py

--- flspstock/models/flsp_customer.py
+++ flspstock/models/flsp_customer.py
@@ -7,4 +7,5 @@ class flspstockcustomer(models.Model):
     _inherit = 'res.partner'
     _check_company_auto = True
 
+
     flsp_show_packing = fields.Boolean(string="Show Packaging on Packing List")
