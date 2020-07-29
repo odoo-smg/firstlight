8a207d9a4cfbdd6a7ebb49fa2917185d2c43c716
odoo-smg <alexandresousa@smartrend.com>
2020-07-29 13:53:06 -0500

    Sales Report

---
+++ flspautoemails/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': "FLSP - Automatic Emails",
+
+    'summary': """
+        This module intend to send reports by email according to the scheduled action.
+        First Light Safety Products""",
+
+    'description': """
+        Customizations performed:
+
+        Daily Sales Order Report:
+            * Material Planners report - used to place the purchase order based on the demand.
+    """,
+
+    'author': "Alexandre Sousa",
+    'website': "http://www.smartrendmfg.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'depends': ['sale_management'],
+
+    # always loaded
+    'data': [
+        'views/dailysalesorder_tmpl.xml',
+        'data/dailysalesorder_cron.xml',
+    ],
+}
---
+++ flspautoemails/model/dailysalesorder.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from odoo import fields, models, api, tools
+
+_logger = logging.getLogger(__name__)
+
+
+class flspdailysalesorder(models.Model):
+    _inherit = 'sale.order'
+    _check_company_auto = True
+
+    @api.model
+    def _dailysalesorder_email(self):
+        template = self.env.ref('flspautoemails.flsp_dailysalesorder_tmpl', raise_if_not_found=False)
+
+        if not template:
+            print('--------------------------template not found')
+            _logger.warning('Template "flspautoemails.flsp_dailysalesorder_tmpl" was not found. Cannot send Daily Sales Order Report.')
+            return
+
+        daily_sales = self.env['sale.order'].search(['&', ('state', '=', 'sale'), ('create_date', '>=', date.today() + relativedelta(days=-1))])
+        total_sales = self.env['sale.order'].search_count(['&', ('state', '=', 'sale'), ('create_date', '>=', date.today() + relativedelta(days=-1))])
+
+        rendered_body = template.render({'sales': daily_sales, 'total_sales': total_sales}, engine='ir.qweb')
+        body = self.env['mail.thread']._replace_local_links(rendered_body)
+
+        if total_sales > 0:
+            self.env['mail.mail'].create({
+                'body_html': body,
+                'subject': 'Daily Sales Order Report',
+                'email_to': 'alexandresousa@smartrendmfg.com',
+                'auto_delete': True,
+            }).send()
+        print('************ Daily Sales Order Report - DONE ******************')
