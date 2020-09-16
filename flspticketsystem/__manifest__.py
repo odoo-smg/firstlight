a1137f512dc2a482205d08cdd0b53d795fab145f
odoo-smg <alexandresousa@smartrend.com>
2020-09-16 12:09:19 -0500

    Ticket second Version

--- flspticketsystem/__manifest__.py
+++ flspticketsystem/__manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sept_4th',
-    'version': '0.1',
+    'version': '1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
--- flspticketsystem/models/ticket.py
+++ flspticketsystem/models/ticket.py
@@ -20,15 +20,17 @@ class Ticket(models.Model):
         help='Enter your user name')
 
     priority = fields.Selection([('P', 'Preventing operation'), ('M', 'Must have'), ('N', 'Nice to have')], string="Priority",
-        required=True, help='Set priority level where:\n* Preventing operation as extremely urgent \n* '
+         required=True, help='Set priority level where:\n* Preventing operation as extremely urgent \n* '
             ' Must have are key missing features in operation \n* '
             ' Nice to have are some suggested improvements for user interaction')
 
-    category_id = fields.Many2one('flspticketsystem.category', ondelete='cascade',
-        string="Category", required=True, help='Choose the department/model your inquiring changes for')
+    category_id = fields.Many2one('flspticketsystem.category', ondelete='cascade', required=True,
+        string="Category",  help='Choose the department/model your inquiring changes for')
 
-    short_description = fields.Char(string="Short Description", required=True, size=80,
+
+    short_description = fields.Char(string="Short Description", size=80, required=True,
         help='Please provide a short description of the request')
+
     detailed_description = fields.Text(string="Detailed Description", required=True,
         help='Please provide a detailed description.\n "A problem well-stated is half-solved" - Charles Kettering')
 
@@ -42,28 +44,35 @@ class Ticket(models.Model):
     analysis_solution = fields.Text(string="Analysis/Solution")
     color = fields.Integer()
 
+    email_user_on_ticket_creation = fields.Boolean(default=True,
+        help='Deactivate if you do not want to send user email upon creation of ticket') #will have to be used as email to user on open
 
-    # active = fields.Boolean(default=True) #will have to be used as email to user on open
-
-    # Will have to handle the email on create
+    # @api.onchange('email_user')
     @api.model
     def create(self, values):
         """
-            Purpose: On creation an email is sent to the user and it team
+            Purpose: On creation an email is sent to the user if boolean is true in admin
+             but if not its not sent upon creation
         """
-        # Code before create: send email on creation
-        body = '<p>Hi there, your ticket has been created.</p>'
-        body += '  <br/>'
-        body += '<p>Thank you!</p>'
-        self.env['mail.mail'].create({
-            'body_html': body,
-            'subject': 'Ticket request Open',
-            'email_to': self.responsible,  #will have to add the admin emails
-            'auto_delete': True,
-        }).send()
-
-        record = super(Ticket, self).create(values)
-        # Code after create
+        if values.get('email_user_on_ticket_creation'):
+            body = '<p>Hi there, your ticket has been created.</p>'
+            body += '  <br/> <p> Access all your tickets on link below<p>'
+            body += '<br/><br/><br/>'
+            body += '<div style = "text-align: center;" >'
+            body += '  <a href = "/web#action=635&model=flspticketsystem.ticket&view_type=list&cids=&menu_id=420" style = "background: #1abc9c; padding: 20px; text-decoration: none; color: #fff; border-radius: 5px; font-size: 16px;" class ="o_default_snippet_text">Access Tickets</a>'
+            body += '  <br/><br/>'
+            body += '</div>'
+            body += '<p>Thank you!</p>'
+            self.env['mail.mail'].create({
+                'body_html': body,
+                'subject': 'Ticket request Open',
+                'email_to': 'ithelpdesk@smgrp.com', #self.responsible,  # will have to add the admin emails e.g iap@odoo.com
+                'email_cc': False,
+                'auto_delete': True,
+            }).send()
+            record = super(Ticket, self).create(values)
+        else:
+            record = super(Ticket, self).create(values)
         return record
 
     @api.onchange('responsible')
@@ -89,15 +98,41 @@ class Ticket(models.Model):
                 body = '<p>Hi there, </p>'
                 body += '<br/>'
                 body += '<p>The ticket containing the following description: ' + self.short_description + ' has been solved.</p>'
-                body += '<br/>'
+                body += '<br/> <p> Access all your tickets on link below<p>'
+                body += '<br/><br/>'
+                body += '<div style = "text-align: center;" >'
+                body += '  <a href = "/web#action=635&model=flspticketsystem.ticket&view_type=list&cids=&menu_id=420" style = "background: #1abc9c; padding: 20px; text-decoration: none; color: #fff; border-radius: 5px; font-size: 16px;" class ="o_default_snippet_text">Access Tickets</a>'
+                body += '  <br/><br/><br/>'
+                body += '</div>'
                 body += '<p>Thank you!</p>'
                 self.env['mail.mail'].create({
                     'body_html': body,
                     'subject': 'Ticket request Closed',
+                    'email_from': 'ithelpdesk@smgrp.com',
                     'email_to': self.requestor.login,
                     'auto_delete': True,
                 }).send()
 
+    # @api.model
+    # def create(self, values):
+    #     """
+    #         Purpose: On creation an email is sent to the user and it team
+    #     """
+    #             # r.active = True
+    #     # Code before create: send email on creation
+    #     body = '<p>Hi there, your ticket has been created.</p>'
+    #     body += '  <br/>'
+    #     body += '<p>Thank you!</p>'
+    #     self.env['mail.mail'].create({
+    #         'body_html': body,
+    #         'subject': 'Ticket request Open',
+    #         'email_to': self.responsible,  #will have to add the admin emails
+    #         'auto_delete': True,
+    #     }).send()
+    #
+    #     record = super(Ticket, self).create(values)
+    #     # Code after create
+    #     return record
 
     ##Not needed yet but i limit the visibility to only the managers
     # def button_reopen(self):
