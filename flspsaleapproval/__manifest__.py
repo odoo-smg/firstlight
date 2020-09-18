5d919ae35a8a543a55a8099816decfe878440d06
odoo-smg <alexandresousa@smartrend.com>
2020-09-18 14:24:26 -0500

    email order confirmed

--- flspsaleapproval/__manifest__.py
+++ flspsaleapproval/__manifest__.py
@@ -41,6 +41,7 @@
         'report/flsp_invoice_report_withoutpay.xml',
         'views/flsp_sppepp_message_view.xml',
         'views/flsp_choose_delivery.xml',
-        'views/flsp_reject_wizard_view.xml',        
+        'views/flsp_reject_wizard_view.xml',
+        'views/flsp_confirmed_order_email.xml',
     ],
 }
