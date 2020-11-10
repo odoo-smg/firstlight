d2def380c2baea095d5759d8242d395889fdf010
odoo-smg <alexandresousa@smartrend.com>
2020-11-10 09:02:42 -0600

    Ticket #92 - MRP Sold Products Report

---
+++ flsp_mrp_sales_report/models/vending_report.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+from odoo import tools
+from odoo import api, fields, models
+
+
+class FlspMrpSalesReport(models.Model):
+    """
+        Class_Name: FlspMrpSalesReports
+        Model_Name: flsp.mrp.sales.report
+        Purpose:    To create a sold product list for use in Manufacturing
+        Date:       Nov/9th/Monday/2020
+        Updated:
+        Author:     Sami Byaruhanga
+    """
+    _name = 'flsp.mrp.sales.report'
+    _auto = False
+    _description = 'Mrp Sales Report'
+
+    # order_id = fields.Many2one('sale.order', 'Order #', readonly=True)
+    order_id = fields.Char('Order Reference', readonly=True)
+    partner_id = fields.Many2one('res.partner', 'Customer', readonly=True)
+
+    country_id = fields.Many2one('res.country', 'Customer Country', readonly=True)
+    street = fields.Char('Customer Street', readonly=True)
+    city = fields.Char('Customer City', readonly=True)
+    zip = fields.Char('Customer Zip', readonly=True)
+
+    commitment_date = fields.Datetime('Delivery Date', readonly=True)
+    date = fields.Datetime('Order Date', readonly=True)
+
+    product_id = fields.Many2one('product.product', 'Product', readonly=True)
+    default_code = fields.Char(string='Part #', readonly=True)
+    qty_delivered = fields.Float('Qty Delivered', readonly=True)
+
+    serial_number = fields.Many2one('stock.production.lot', readonly=True)
+
+    def init(self):
+        """
+            Purpose: To extract database information and create tree view using the query
+        """
+        tools.drop_view_if_exists(self._cr, 'flsp_mrp_sales_report')
+
+        query = """
+        CREATE or REPLACE VIEW flsp_mrp_sales_report AS (
+        SELECT
+            sml.id as id,
+            rp.id as partner_id,
+            so.name as order_id,
+            sm.product_id as product_id,
+            pt.default_code as default_code,
+            sml.qty_done as qty_delivered,
+            so.commitment_date as commitment_date,
+            so.date_order as date,
+            spl.id as serial_number,
+            rp.street as street,
+            rp.zip as zip,
+            rp.city as city,
+            rc.id as country_id
+        FROM stock_move as sm
+            inner join 	stock_move_line as sml
+            on 			sml.move_id = sm.id
+            inner join 	product_template as pt
+            on			sm.product_id = pt.id
+            left join 	stock_production_lot as spl
+            on 			sml.lot_id = spl.id
+            inner join 	stock_picking as sp
+            on 			sp.id = sm.picking_id
+            inner join 	sale_order as so
+            on 			so.id = sp.sale_id
+            inner join 	res_partner as rp
+            on 			so.partner_id = rp.id and so.state = 'sale'
+            inner join 	res_country as rc
+            on 			rp.country_id = rc.id
+        );
+        """
+        self.env.cr.execute(query)
+
