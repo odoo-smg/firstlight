e4a938d6f28fae906e0d1e69c66d5ba622623bbb
odoo-smg <alexandresousa@smartrend.com>
2021-02-10 11:36:02 -0600

    Ticket #240 Drop-shipping - Warranty

---
+++ flsppurchase_warranty/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "FLSP - Purchase Warranty",
+
+    'summary': """
+        Model purpose is to handle FLSP purchase warranty""",
+
+    'description': """
+        Customizations performed:
+            * Feb/10th/2021/W - Purchase form: added warranty button check box
+    """,
+    'author': "Sami Byaruhanga",
+    'website': "http://www.firstlightsafety.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'purchase'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/flsppurchase_warranty.xml',
+    ],
+}
