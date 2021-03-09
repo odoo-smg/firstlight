b34d4c36754d3ca5bd8ffb4e5f5dc07f924cbd28
odoo-smg <alexandresousa@smartrend.com>
2021-03-09 13:32:50 -0600

    Ticket #306 - Sales Target

---
+++ flsp_sale_target/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': "FLSP Sale Target",
+
+    'summary': """
+        To create a sales target by product category """,
+
+    'description': """
+        Customizations performed:
+
+        Product Category:
+            * Creates a list of sales target by month.
+    """,
+
+    'author': "Alexandre Sousa",
+    'website': "http://www.smartrendmfg.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock', 'sale'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/flsp_category_target.xml',
+        'views/partner_tags.xml',
+    ],
+}
---
+++ flsp_sale_target/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+from . import flsp_product_category
+from . import flsp_sale_target_category
