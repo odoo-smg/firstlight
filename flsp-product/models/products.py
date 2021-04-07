d5218b57de9a94fe7793fad224acf483dd4bacd3
odoo-smg <alexandresousa@smartrend.com>
2021-04-07 10:59:42 -0500

    Ticket #325: ECO Reject

--- flsp-product/models/products.py
+++ flsp-product/models/products.py
@@ -125,53 +125,82 @@ class Smgproduct(models.Model):
             },
         }
 
+    #Modified Apr.7th.2021.W by Sami to account for wheather the product has default values
     def copy(self, default=None):
         flsp_default_part_init = self.env['ir.config_parameter'].sudo().get_param('product.template.flsp_part_init')[:1]
         default = dict(default or {})
-        # PREFIX INFORMATION
-        if not(self.flsp_part_prefix):
+
+        # Useful for Products with default values set in this case flsp-eco-reject
+        if default:
             prefix = '00000'
+            self._cr.execute(
+                "select max(flsp_part_suffix) as suffix from product_template where active ='false'")
+            table = self._cr.fetchall()
+            for line in table:
+                if type(line[0]) == str:
+                    value = int(line[0])
+                    suf = str(value + 1) + ""
+                else:
+                    suf = '0'
+            suffix = suf
+            default['default_code'] = '9'+prefix+'-'+suffix
+            default['flsp_part_suffix'] = suffix
+            default['flsp_part_prefix'] = prefix
+            return super(Smgproduct, self).copy(default)
+
         else:
-            prefix = self.flsp_part_prefix
-        # SUFFIX INFORMATION
-        self._cr.execute(
-            "select max(flsp_part_suffix) as suffix from product_template where flsp_part_prefix like '" + self.flsp_part_prefix + "%' and length(default_code) = 10 ")
-        table = self._cr.fetchall()
-        for line in table:
-            value = int(line[0])
-        suf = value
-        if not(self.flsp_part_suffix):
-            suffix = '000'
-        else:
-            # suffix = str(int(self.flsp_part_suffix)+1)+""
-            suffix = str(suf+1)+""
-        suffix = ('000'+str(int(suffix)))[-3:]
-        default_init = self.default_code[:1]
-        default['default_code'] = default_init+prefix+'-'+suffix
-        default['flsp_part_suffix'] = suffix
-        default['flsp_part_prefix'] = prefix
-        if 'flsp_plm_valid' in self.env['product.template']._fields:
-            default['flsp_plm_valid'] = False
-        return super(Smgproduct, self).copy(default)
+            # PREFIX INFORMATION
+            if not(self.flsp_part_prefix):
+                prefix = '00000'
+            else:
+                prefix = self.flsp_part_prefix
+
+            # SUFFIX INFORMATION
+            self._cr.execute(
+                "select max(flsp_part_suffix) as suffix from product_template where flsp_part_prefix like '" + self.flsp_part_prefix + "%'")
+                # "select max(flsp_part_suffix) as suffix from product_template where flsp_part_prefix like '" + self.flsp_part_prefix + "%' and length(default_code) = 10 ")
+            table = self._cr.fetchall()
+            for line in table:
+                value = int(line[0])
+            suf = value
+
+            if not(self.flsp_part_suffix):
+                suffix = '000'
+            else:
+                # suffix = str(int(self.flsp_part_suffix)+1)+""
+                suffix = str(suf+1)+""
+
+            suffix = ('000'+str(int(suffix)))[-3:]
+            default_init = self.default_code[:1]
+
+            default['default_code'] = default_init+prefix+'-'+suffix
+            default['flsp_part_suffix'] = suffix
+            default['flsp_part_prefix'] = prefix
+            if 'flsp_plm_valid' in self.env['product.template']._fields:
+                default['flsp_plm_valid'] = False
+            return super(Smgproduct, self).copy(default)
 
 
-    @api.constrains('name','flsp_part_prefix')
+    @api.constrains('name') #,'flsp_part_prefix')
     def _constraint_only_unique_name(self):
         """
             Date:    Nov/19th/2020/Thursday
             Purpose: To create only unique names
                      To raise exception if name is used but prefix don't match
+	    Modified:Apr/7th/W to account for whether the product is active
             Author: Sami Byaruhanga
         """
         self._cr.execute('''
-            SELECT id pdct_id, name, flsp_part_prefix, default_code from product_template
+            SELECT id pdct_id, name, flsp_part_prefix, default_code, active from product_template
+
         ''')
+
         res = self._cr.fetchall() #returns all
         for line in res:
-            # print(line)
-            # print("name=", line[1], "flsp_part_prefix=", line[2])
-            if self.name == line[1] and self.flsp_part_prefix != line[2]:
-                raise ValidationError('Name already used on product with default code: ' + line[3] +
-                      '\nTo use this name you must use the matching Part # Prefix: ' + line[2] +
-                      ' and increment the Part # Suffix'
-                      )
+            if line[4]: #accoung for whether the product is active
+                if self.name == line[1] and self.flsp_part_prefix != line[2]:
+                    raise ValidationError('Name already used on product with default code: ' + line[3] +
+                                      '\nTo use this name you must use the matching Part # Prefix: ' + line[2] +
+                                      ' and increment the Part # Suffix'
+                                      )
+
---
+++ flsp_eco_reject/models/flsp_eco_reject.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from flspArchieveBase.mrp_plm.models.mrp_eco import MrpEco
+from odoo import models, fields, api
+
+
+class FlspEcoReject(models.Model):
+    """
+        class_name: FlspEcoReject
+        model_name: mrp.eco
+        Purpose:    To create a product backup to be used when rejecting an ECO Stage
+        Date:       April.7th.2021.W
+        Author:     Sami Byaruhanga
+    """
+    _inherit = 'mrp.eco'
+
+    backup_pdct = fields.Many2one('product.template', string='Back up product', compute='create_backup', store=True)
+
+    @api.depends('state')
+    def create_backup(self):
+        """"
+            Purpose:    To create back up product based on state and product_templ_id
+                        Unlink backup when the ECO Is done
+        """
+        for record in self:
+            if record.state == 'confirmed':
+                if record.product_tmpl_id:
+                    print("We have confirmed state")
+                    backup_pdct = record.product_tmpl_id.copy(default={'active': False,
+                                                                       'name': record.product_tmpl_id.name  + ' ',
+                                                                       })
+                    record.backup_pdct = backup_pdct
+
+            if record.state == 'done' and record.backup_pdct:
+                print("ECO IS Done")
+                record.backup_pdct.unlink()
+
+    def reject(self):
+        """
+            Purpose:    To write the product to intial state
+            Note:       Method inherited from parent class add the product_tmpl_id.write
+        """
+        self._create_or_update_approval(status='rejected')
+        self.product_tmpl_id.write({'name': self.backup_pdct.name,
+                                    'type': self.backup_pdct.type,
+                                    'categ_id': self.backup_pdct.categ_id,
+                                    'list_price': self.backup_pdct.list_price,
+                                    'taxes_id': self.backup_pdct.taxes_id,
+                                    'standard_price': self.backup_pdct.standard_price,
+                                    'uom_id': self.backup_pdct.uom_id.id,
+                                    'tracking': self.backup_pdct.tracking,
+                                    'attachment_ids': self.backup_pdct.attachment_ids,
+                                    #Make active post sending to alex TODO
+                                    # 'x_studio_specification': self.backup_pdct.x_studio_specification,
+                                    # 'x_studio_drawing': self.backup_pdct.x_studio_drawing,
+                                    })
+
+
+"""
+    In case we need for plm 
+    'volume': self.backup_pdct.volume,
+    'legacy_code': self.backup_pdct.legacy_code,
+    'weight': self.backup_pdct.weight,
+    'sale_ok': self.backup_pdct.sale_ok,
+    'purchase_ok': self.backup_pdct.purchase_ok,
+    'uom_po_id': self.backup_pdct.uom_po_id,
+    'flsp_specification': self.backup_pdct.flsp_specification,
+    'flsp_tags_id': self.backup_pdct.flsp_tags_id,
+    'flsp_manufacturer': self.backup_pdct.flsp_manufacturer,
+    'flsp_manufacture_part_number': self.backup_pdct.flsp_manufacture_part_number,
+"""
