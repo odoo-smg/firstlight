d8b844abad169f487c9e61819281578e8964c40a
odoo-smg <alexandresousa@smartrend.com>
2021-05-19 18:16:31 -0500

    Ticket #411 - BOM Structure and qty

---
+++ flsp_mrp_structure/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "FLSP - MRP Structure",
+
+    'summary': """
+        Show the needed quantities to complete the MO and track WHEN the components will arrive""",
+
+    'description': """
+        Show the needed quantities to complete the MO and track WHEN the components will arrive
+    """,
+
+    'author': "Alexandre Sousa",
+    'website': "http://www.smartrendmfg.com",
+    "category": "Manufacture",
+    'version': '0.1',
+    'depends': ['base', 'mrp', 'sale', 'purchase', 'stock'],
+
+    # always loaded
+    'data': [
+        'views/flsp_mrp_structure.xml',
+        'views/production_form_view.xml',
+    ],
+    'qweb': ['static/src/xml/flsp_mrp.xml'],
+}
