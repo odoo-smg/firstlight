344be113301729a863d3b64ab6c16d03363438f7
perrysmg <80538049+perrysmg@users.noreply.github.com>
2021-05-28 09:15:06 -0500

    Ticket 415:  refactor

--- flsp_mrp_filter_sn/models/filter_sn_in_mo.py
+++ flsp_mrp_filter_sn/models/filter_sn_in_mo.py
@@ -20,14 +20,14 @@ class FlspMrpProduceLineFilterLot(models.TransientModel):
     _inherit = 'mrp.product.produce.line'
 
     def _get_default_lots(self):
-        res = []
+        res = False
         mo_id = self.env.context.get('default_mo_id')
         if mo_id:
             mo = self.env['mrp.production'].browse(mo_id)
-            res = self.env['stock.quant'].search([('location_id', '=', mo.location_src_id.id), ('product_id', '=', self.product_id.id), ('quantity', '>', 0.0), '|', ('company_id', '=', False), ('company_id', '=', self.company_id.id)]).lot_id
-        else:
+            if mo.exists():
+                res = self.env['stock.quant'].search([('location_id', '=', mo.location_src_id.id), ('product_id', '=', self.product_id.id), ('quantity', '>', 0.0), '|', ('company_id', '=', False), ('company_id', '=', self.company_id.id)]).lot_id
+        if not res:
             res = self.env['stock.quant'].search([('product_id', '=', self.product_id.id), ('quantity', '>', 0.0), '|', ('company_id', '=', False), ('company_id', '=', self.company_id.id)]).lot_id
-
         return res
     
     lot_candidates = fields.Many2many('stock.production.lot', string='Lots', default=_get_default_lots)
