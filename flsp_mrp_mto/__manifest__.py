c2e5bcbef664bf17414bbc72db6e68af293c8ccd
odoo-smg <alexandresousa@smartrend.com>
2021-06-02 07:48:28 -0500

    Ticket #425 MTO Report

---
+++ flsp_mrp_mto/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "FLSP - MRP MTO",
+
+    'summary': """
+        Show the needed quantities to produce from Sales Deliveries""",
+
+    'description': """
+        Show the needed quantities to produce from Sales Deliveries
+    """,
+
+    'author': "Alexandre Sousa",
+    'website': "http://www.smartrendmfg.com",
+    "category": "Manufacture",
+    'version': '0.1',
+    'depends': ['base', "mrp", "stock", "sale"],
+
+    # always loaded
+    'data': [
+        'views/flsp_mrp_mto.xml',
+    ],
+}
