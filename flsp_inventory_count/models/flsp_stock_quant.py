c07df42c19161f8e8a38f2af1436b17dde5403fa
odoo-smg <alexandresousa@smartrend.com>
2021-06-08 08:16:51 -0500

    Ticket: #443 Inventory Permissions

--- flsp_inventory_count/models/flsp_stock_quant.py
+++ flsp_inventory_count/models/flsp_stock_quant.py
@@ -8,3 +8,10 @@ class FlspStockQuant(models.Model):
     _check_company_auto = True
 
     default_code = fields.Char(String='Product', related="product_id.default_code")
+    flsp_check_inv = fields.Boolean('Can edit', compute='_compute_flsp_inv')
+
+    def _compute_flsp_inv(self):
+        if self.user_has_groups('flsp_inventory_count.group_iventory_count_manager'):
+            self.flsp_check_inv = True
+        else:
+            self.flsp_check_inv = False
