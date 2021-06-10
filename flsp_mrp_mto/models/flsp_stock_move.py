e19cdcbd17b4fa934832027ca8c5aa31cb3fb348
odoo-smg <alexandresousa@smartrend.com>
2021-06-10 15:55:11 -0500

    Ticket #452 - Library timedelta

--- flsp_mrp_mto/models/flsp_stock_move.py
+++ flsp_mrp_mto/models/flsp_stock_move.py
@@ -2,6 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from datetime import timedelta
+import dateutil.rrule as rrule
 from datetime import datetime
 from odoo.exceptions import UserError
 from odoo.exceptions import ValidationError
