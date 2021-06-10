6f5279883258a9720bad61bd677b6f8f1179644e
odoo-smg <alexandresousa@smartrend.com>
2021-06-10 12:53:08 -0500

    Ticket #391: Sales Report by Item

---
+++ flsp_sales_item_report/models/flsp_sale_order_line.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+
+class flspsalesorderline(models.Model):
+    _inherit = 'sale.order.line'
+
+    flsp_partner_id = fields.Many2one('res.partner', 'Customer', store=True, readonly=True, related="order_id.partner_id")
+    flsp_ship_date = fields.Datetime('Shipping Date', readonly=True, srelated="order_id.commitment_date")
+    flsp_bpm_status = fields.Selection([
+        ('quote', 'Quote'),
+        ('wait', 'Waiting Approval'),
+        ('approved', 'Disc.Approved'),
+        ('sale', 'Sales Order'),
+        ('confirmed', 'Delivery Confirmed'),
+        ('packed', 'Packed'),
+        ('shipped', 'Shipped'),
+        ('partial', 'Partially Shipped'),
+        ('tracking', 'Tracking Assigned'),
+        ('delivered', 'Delivered'),
+        ('cancel', 'Cancelled'),
+        ('aa-quote', 'Quote'),
+        ('bb-wait', 'Waiting Approval'),
+        ('cc-approved', 'Disc.Approved'),
+        ('dd-sale', 'Sales Order'),
+        ('ee-confirmed', 'Shipping Confirmed'),
+        ('ff-packed', 'Packed'),
+        ('gg-partial', 'Partially Shipped'),
+        ('hh-shipped', 'Shipped'),
+        ('ii-tracking', 'Tracking Assigned'),
+        ('jj-delivered', 'Delivered'),
+        ('kk-cancel', 'Cancelled'), ], string='Status', readonly=True, related="order_id.flsp_bpm_status")
