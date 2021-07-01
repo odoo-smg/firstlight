d54cf0650805319660c123fb1b035fa819a910d4
odoo-smg <alexandresousa@smartrend.com>
2021-07-01 14:58:53 -0500

    Update flsp_stock_picking.py

--- flsp_mrp_wip/models/flsp_stock_picking.py
+++ flsp_mrp_wip/models/flsp_stock_picking.py
@@ -9,6 +9,5 @@ _logger = logging.getLogger(__name__)
 
 class flspmrpwipproduction(models.Model):
     _inherit = 'stock.picking'
-    _check_company_auto = True
 
     flsp_mo_wip_id = fields.Many2one('mrp.production', string="MO")
