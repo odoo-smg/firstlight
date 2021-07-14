7b86cc987e5162ff0989810a36234f253985a64b
odoo-smg <alexandresousa@smartrend.com>
2021-07-14 12:33:17 -0500

    Update product_product.py

--- flsp_mrp_purchase/models/product_product.py
+++ flsp_mrp_purchase/models/product_product.py
@@ -13,19 +13,11 @@ class mrppruchaseprd(models.Model):
         action['domain'] = [('product_id', '=', self.id)]
         return action
 
-        #view_id = self.env.ref('flsp_mrp_wip.flsp_mrp_wip_wiz_form_view').id
+class flsppurchaseproductprdtmpl(models.Model):
+    _inherit = 'product.template'
+    _check_company_auto = True
 
-        return {
-            'name': 'WIP Transfer',
-            'type': 'ir.actions.act_window',
-            'view_mode': 'form',
-            'res_model': 'flsp.mrp.wip.wiz',
-            'view_id': view_id,
-            'views': [(view_id, 'form')],
-            'target': 'new',
-            'context': {
-                'default_mo_id': self.mo_id.id,
-                'show_sublevels': self.show_sublevels,
-                'wip_id': wip_id,
-            }
-        }
+    def action_view_mrp_purchase(self):
+        action = self.env.ref('flsp_mrp_purchase.flsp_mrp_purchase_line_action').read()[0]
+        action['domain'] = [('product_tmpl_id', '=', self.id)]
+        return action
