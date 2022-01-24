0fa1922eb0e662d99b496057f0ebc9d14ffe8c56
KorySMG <93681355+KorySMG@users.noreply.github.com>
2022-01-24 10:41:21 -0600

    Update salesorder.py

--- flsp-salesorder/models/salesorder.py
+++ flsp-salesorder/models/salesorder.py
@@ -124,7 +124,7 @@ class flspsalesorderline(models.Model):
     @api.onchange('product_id')
     def flsp_product_id_onchange(self):
         for rec in self:
-            return {'domain': {'customerscode_ids': [('partner_id', '=', rec.order_id.partner_id)]}}
+            return {'domain': {'customerscode_ids': [('product_id', '=', rec.product_id)]}}
     
     @api.onchange('product_uom_qty')
     def flsp_product_uom_qty_onchange(self):
