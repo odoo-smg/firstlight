e216d8645b1946a030f48714bbf48c3767230def
Alexandre Sousa <alexandresousa@smartrendmfg.com>
2022-04-28 13:50:28 -0500

    Ticket #773 - Receiving Container

--- flsp_purchase_container/models/flsp_purchase_container.py
+++ flsp_purchase_container/models/flsp_purchase_container.py
@@ -42,6 +42,72 @@ class Container(models.Model):
         action['domain'] = [('picking_id', 'in', self.container_lines.picking_id.ids)]
         return action
 
+    def container_receive(self):
+        for line in self.container_lines:
+            self.receive_receipt(line.picking_id)
+        self.status = 'received'
+
+    def receive_receipt(self, picking_id):
+        if not picking_id:
+            return
+        if picking_id.state == 'done':
+            return
+        for move in picking_id.move_ids_without_package:
+            self.create_stock_move_line(move)
+        picking_id.button_validate()
+
+    def create_stock_move_line(self, move_id):
+        if not move_id:
+            return
+
+        if move_id.product_id.tracking == 'none':
+            lot_serial_id = False
+        elif move_id.product_id.tracking == 'lot':
+            lot_serial_id = self.create_lot(move_id.product_id)
+        else: ## serial - do not receive serial
+            return
+
+        stock_move_line = self.env['stock.move.line'].create({
+            'product_id': move_id.product_id.id,
+            'product_uom_id': move_id.product_uom.id,
+            'qty_done': move_id.product_uom_qty,
+            'picking_id': move_id.picking_id.id,
+            'reference': move_id.picking_id.name,
+            'move_id': move_id.id,
+            'location_id': move_id.location_id.id,
+            'location_dest_id': move_id.location_dest_id.id,
+            'lot_id': lot_serial_id,
+        })
+
+    def create_lot(self, product_id):
+        if product_id:
+            part_init = product_id.default_code[0:6]
+            self._cr.execute(
+                "select max(name) as code from stock_production_lot where name like '" + part_init + "%' and length(name) = 13")
+            retvalue = self._cr.fetchall()
+            returned_registre = retvalue[0]
+            nextseqnum = self._get_next_seqnum(returned_registre[0])
+            nextlotnum = part_init
+            nextlotnum = nextlotnum + "_"
+            nextlotnum = nextlotnum + nextseqnum
+
+            lot = self.env['stock.production.lot'].create({
+                'name': nextlotnum,
+                'product_id': product_id.id,
+                'company_id': self.env.company.id})
+            return lot.id
+        else:
+            return False
+
+    def _get_next_seqnum(self, currpartnum):
+        if not currpartnum:
+            retvalue = '000001'
+        else:
+            retvalue = ('00000' + str(int(currpartnum[-5:]) + 1))[-6:]
+
+        return retvalue
+
+
     def purchase_wizard(self):
         action = self.env.ref('flsp_purchase_container.launch_flsp_purchase_container_po_wiz').read()[0]
         return action
@@ -70,4 +136,3 @@ class ContainerLine(models.Model):
     container_id = fields.Many2one('flsp.purchase.container', required=True)
     purchase_id = fields.Many2one('purchase.order', string='PO', required=True)
     picking_id = fields.Many2one('stock.picking', string='Receipt', required=True)
-
