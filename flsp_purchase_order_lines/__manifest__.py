370f0b98d32f331b920569fb256864c9ffef3b3a
Alexandre Sousa <alexandresousa@smartrendmfg.com>
2022-06-15 13:49:58 -0500

    Ticket #596 - PO Lines Report

---
+++ flsp_purchase_order_lines/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "FLSP - Purchase Lines Report",
+
+    'summary': """
+        Purchase Lines Report.""",
+
+    'description': """
+        Purchase Lines Report.
+    """,
+    'author': "Mowahid Latif",
+    'website': "http://www.firstlightsafety.com",
+
+    'category': 'purchase',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'purchase'],
+
+    # always loaded
+    'data': [
+        'views/flsp_po_lines_view.xml',
+    ],
+
+    'installable': True,
+}
