75f0b2f8ad77d32991f16d84d49cc601977344c1
Alexandre Sousa <alexandresousa@smartrendmfg.com>
2022-09-16 13:35:10 -0500

    dependency

--- flsp_customer_badge/__manifest__.py
+++ flsp_customer_badge/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'purchase', 'account_accountant'],
+    'depends': ['base', 'sale', 'mrp', 'purchase', 'account_accountant'],
 
     'assets': {
             'web.assets_backend': [
